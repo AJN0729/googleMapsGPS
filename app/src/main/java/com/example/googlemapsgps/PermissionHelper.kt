0f4f3e000923abd3ec0f479072e7419a277a1052
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-03-21 10:46:37 -0500

    added some map stuff.

---
+++ app/src/main/java/com/example/googlemapsgps/PermissionHelper.kt
@@ -0,0 +1,27 @@
+package com.example.googlemapsgps
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object PermissionHelper {
+
+    fun askForLocationPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(activity, PERMISSIONS, REQUEST_CODE_FOR_PERMISSION)
+    }
+
+    fun arePermissionGranted(context: Context) =
+        PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(context, it)==PackageManager.PERMISSION_GRANTED
+        }
+
+    const val REQUEST_CODE_FOR_PERMISSION = 0x1001
+
+    private val PERMISSIONS = arrayOf(
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+
+    )
+}
