d293536e532d8e76b34cb69e49c3de392dad7df5
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-03-22 10:29:39 -0500

    done

---
+++ app/src/main/java/com/example/googlemapsgps/LocationModel.kt
@@ -0,0 +1,8 @@
+package com.example.googlemapsgps
+
+import android.location.Location
+
+data class LocationModel(
+    val location: Location,
+    val timeStamp: Long
+)
--- app/src/main/java/com/example/googlemapsgps/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsgps/MapsActivity.kt
@@ -1,8 +1,13 @@
 package com.example.googlemapsgps
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.MutableLiveData
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,9 +16,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.googlemapsgps.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -30,13 +34,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         priority = LocationRequest.PRIORITY_HIGH_ACCURACY
     }
 
+    private lateinit var locationCallback: LocationCallback
+
+    private val logTAG = MapsActivity::class.java.simpleName
+
+    var mCurrentLocation = MutableLiveData<LocationModel>()
+
     fun startLocationTracking(context: Context) {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-        mFusedLocationClient.requestLocationUpdates()
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(locations: LocationResult) {
+                super.onLocationResult(locations)
+                for(location in locations.locations) {
+                    mCurrentLocation.postValue(LocationModel(location, Calendar.getInstance().timeInMillis))
+                }
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            PermissionHelper.askForLocationPermission(this)
+            return
+        }
+//        mFusedLocationClient.requestLocationUpdates(locationCallbackRequest, locationCallback, null)
     }
 
     fun stopLocationTracking() {
+        mFusedLocationClient.removeLocationUpdates(locationCallback)
+    }
 
+    override fun onPause() {
+        super.onPause()
+        stopLocationTracking()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPermissionAndSetView()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,8 +83,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        checkPermissionAndSetView()
 
     }
 
@@ -62,9 +99,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        mCurrentLocation.observe(this, androidx.lifecycle.Observer {
+            Log.e(logTAG, "${it.location.latitude} ${it.location.longitude}, ${it.timeStamp}")
+
         val tulsa = LatLng(36.1540, -95.9928)
+
         mMap.addMarker(MarkerOptions().position(tulsa).title("Marker in Tulsa"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(tulsa))
+        })
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+             this,
+             Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.setMinZoomPreference(6f)
+        mMap.setMaxZoomPreference(50f)
+
+        val tulsaTechRiverside = LatLng(36.0346, -95.9281)
+        mMap.addMarker(MarkerOptions().position(tulsaTechRiverside).title("My School"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tulsaTechRiverside))
     }
 
     override fun onRequestPermissionsResult(
